eafb04a6472a63e8e341c4d89341511e7514fa50
Janet Iqal <j.iqal35@gmail.com>
2021-10-15 14:11:27 -0500

    deployment

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(require("./routes/homeRoutes.js"));
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true ,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 const db = mongoose.connection
